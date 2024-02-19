025ae8c3f6931c0db9d130c4c13d29abf850a413
eiathom <eiathom@protonmail.ch>
2024-02-19 22:03:20 +0000

    Improves Prettier Configuration (#1)

--- esbuild.js
+++ esbuild.js
@@ -11,8 +11,7 @@ const sharedConfig = {
   entryPoints: ["src/index.ts"],
   bundle: true,
   minify: true,
-  external: Object.keys(dependencies),
-  // external: Object.keys(dependencies).concat(Object.keys(peerDependencies)),
+  external: Object.keys(dependencies).concat(Object.keys(peerDependencies)),
 };
 
 build({
